ce20b7922e2399e6cb0e70516335d0efb9f9a3cb
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-04-30 16:46:52 -0300

    feat: criando api de autenticação com o servidor

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.POST("/auth", auth)
+	router.GET("/healthcheck", healthCheck)
+	router.Run(":8000")
+}
+
+func healthCheck(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "Ta funfando viu, pode ficar tranquilo!")
+}
+
+func auth(c *gin.Context) {
+	log.Println("Running Auth...")
+	body := c.Request.Body
+	defer body.Close()
+
+	fields, _ := io.ReadAll(body)
+	fmt.Println(string(fields))
+
+	c.IndentedJSON(http.StatusOK, "Autenticado com sucesso!")
+}
